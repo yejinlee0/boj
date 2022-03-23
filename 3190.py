2dd119202609d7784b46a2805876896cd76d24f5
Yejin Lee <56724944+yejinlee0@users.noreply.github.com>
2022-03-23 18:18:01 +0900

    Create 3190.py

---
+++ 3190.py
@@ -0,0 +1,56 @@
+from collections import deque
+n = int(input())
+k = int(input())
+grid = [[0 for _ in range(n)] for _ in range(n)]
+for _ in range(k):
+    r, c = map(int, input().split())
+    grid[r-1][c-1] = 1
+L = int(input())
+change = []
+for _ in range(L):
+    tmp1, tmp2 = input().split()
+    change.append([int(tmp1), tmp2])
+
+length = 1
+hr, hc = 0, 0
+body = deque()
+body.append([0,0])
+
+# 오 아래 왼 위 - 시계 방향
+move_dir = [[0,1],[1,0],[0,-1],[-1,0]]
+time = 0
+dir = 0
+idx = 0
+
+def in_range(r, c):
+    if 0<=r<n and 0<=c<n:
+        return 1
+    return 0
+
+while True:
+    time += 1
+
+    nr, nc = hr + move_dir[dir][0], hc + move_dir[dir][1]
+
+    if (in_range(nr, nc) == 0) or ([nr,nc] in body):
+        break
+
+    body.append([nr, nc])
+    if grid[nr][nc] == 1:
+        grid[nr][nc] = 0
+        length += 1
+    else:
+        body.popleft()
+
+    if idx < L and time == change[idx][0]:
+        if change[idx][1] == 'L':
+            dir = dir - 1
+            if dir == -1:
+                dir = 3
+        else:
+            dir = (dir + 1) % 4
+        idx += 1
+
+    hr, hc = nr, nc
+
+print(time)
